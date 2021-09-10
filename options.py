aad8e7f766316bf0f112de30b695c7bf97b19842
wushiqiang2018@ia.ac.cn <wushiqiang2018@ia.ac.cn>
2021-09-10 17:52:53 +0800

    w

--- options.py
+++ options.py
@@ -1,6 +1,7 @@
 class Options:
-    def __init__(self, data_dir: str, peaq_test_dir: str, watermark_length: int, bit_per_seq: int) -> None:
+    def __init__(self, data_dir: str, peaq_test_dir: str, watermark_length: int, bit_per_seq: int, isCode: bool) -> None:
         self.data_dir = data_dir
         self.peaq_test_dir = peaq_test_dir
         self.watermark_length = watermark_length
         self.bit_per_seq = bit_per_seq
+        self.isCode = isCode
