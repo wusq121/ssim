d7652944352136a7c1c2789f7cf1a40cac9d6e61
wushiqiang2018@ia.ac.cn <wushiqiang2018@ia.ac.cn>
2021-09-10 17:03:18 +0800

    first commit

---
+++ embeder.py
@@ -0,0 +1,3 @@
+class Embeddor:
+    def __init__(self) -> None:
+        pass
---
+++ options.py
@@ -0,0 +1,3 @@
+class Options:
+    def __init__(self, data_dir, peaq_test_dir, watermark_length, bit_per_seq) -> None:
+        pass
